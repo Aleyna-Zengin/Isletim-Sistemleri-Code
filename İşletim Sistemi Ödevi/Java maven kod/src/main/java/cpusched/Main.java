5ddab6da3542f9b882f09a7bb621f092dcb433ae
Aleyna-Zengin <aleynazngn.0@gmail.com>
2025-12-14 21:44:35 +0300

    Use ExecutorService to run scheduling algorithms
    Refactor main method to use ExecutorService for scheduling algorithms.

--- İşletim Sistemi Ödevi/Java maven kod/src/main/java/cpusched/Main.java
+++ İşletim Sistemi Ödevi/Java maven kod/src/main/java/cpusched/Main.java
@@ -1,42 +1,23 @@
 package cpusched;
 
-import java.nio.file.*;
-import java.util.*;
-import java.io.*;
+import scheduler.thread.AlgorithmTask;
+import java.util.concurrent.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        if (args.length < 2) {
-            System.out.println("Usage: java -cp target/classes cpusched.Main <input.csv> <output-prefix> [quantum]"); 
-            System.out.println("Example: mvn compile && java -cp target/classes cpusched.Main src/main/resources/case1.csv results/case1 4");
-            return;
-        }
-        String csv = args[0];
-        String outp = args[1];
-        double quantum = args.length >=3 ? Double.parseDouble(args[2]) : 4.0;
-        List<ProcessInfo> list = CSVLoader.load(csv);
 
-        List<Scheduler> scheds = new ArrayList<>();
-        scheds.add(new FCFS());
-        scheds.add(new SJFPreemptive());
-        scheds.add(new SJFNonPreemptive());
-        scheds.add(new RoundRobin(quantum));
-        scheds.add(new PriorityPreemptive());
-        scheds.add(new PriorityNonPreemptive());
+    public static void main(String[] args) {
 
-        // create output dir
-        Path outDir = Paths.get(outp);
-        if (!Files.exists(outDir)) Files.createDirectories(outDir);
+        ExecutorService executor = Executors.newFixedThreadPool(6);
 
-        // run each scheduler sequentially and write results
-        for (Scheduler s : scheds) {
-            SchedulerResult res = s.run(list);
-            String prefix = outp + "/" + s.getName().replace(' ','_').replace('/','_');
-            ResultWriter.write(prefix, res);
-            System.out.println("Wrote results for: " + s.getName());
-        }
+        executor.submit(new AlgorithmTask("FCFS", "src/main/resources/case1.csv"));
+        executor.submit(new AlgorithmTask("SJF_PREEMPTIVE", "src/main/resources/case1.csv"));
+        executor.submit(new AlgorithmTask("SJF_NON_PREEMPTIVE", "src/main/resources/case1.csv"));
+        executor.submit(new AlgorithmTask("RR", "src/main/resources/case1.csv"));
+        executor.submit(new AlgorithmTask("PRIORITY_PREEMPTIVE", "src/main/resources/case1.csv"));
+        executor.submit(new AlgorithmTask("PRIORITY_NON_PREEMPTIVE", "src/main/resources/case1.csv"));
+
+        executor.shutdown();
     }
 }
-
 
 //Aleyna Zengin
